7daaf2f519dd35bdcd59616ba3947b436f73fb0c
Serge Matveenko <s@matveenko.ru>
2014-03-12 20:29:40 +0400

    Generate activation key.

--- regme/documents.py
+++ regme/documents.py
@@ -1,8 +1,15 @@
+from hashlib import sha1
+from random import random
+
+from django.conf import settings
 from mongoengine.django.auth import User as BaseUser
 from mongoengine.signals import pre_save
 
 
 class User(BaseUser):
+
+    def get_activation_key(self):
+        return sha1((settings.SECRET_KEY + str(random())).encode()).hexdigest()
 
     @staticmethod
     def ensure_inactive(klass, document):
--- tests/test_documents.py
+++ tests/test_documents.py
@@ -1,3 +1,5 @@
+from random import seed
+
 from django.conf import settings
 from mongoengine import connect
 import pytest
@@ -32,7 +34,22 @@ def db(User):
     yield
 
 
-def test_create_inactive_user(db, User):
-    user = User.create_user('username', 'password', 'email@example.com')
+@pytest.fixture
+def user_data():
+    return {
+        'username': 'username',
+        'password': 'password',
+        'email': 'email@example.com'}
+
+
+def test_create_inactive_user(db, User, user_data):
+    user = User.create_user(**user_data)
     assert isinstance(user, User)
     assert not user.is_active
+
+
+def test_generate_activation_key(db, User, user_data):
+    seed('')
+    user = User.create_user(**user_data)
+    key = user.get_activation_key()
+    assert key == '516bb9061d58280acd0c3900e18feaf5166f02ff'
